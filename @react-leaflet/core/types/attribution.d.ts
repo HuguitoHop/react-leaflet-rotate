7082d99da994a88687cb50a967bd5e2bf317149e
HuguitoHop <hugoarmoreira@hotmail.com>
2021-12-07 11:02:43 -0300

    message

--- @react-leaflet/core/types/attribution.d.ts
+++
@@ -1,2 +0,0 @@
-import { Map } from 'leaflet-rotate-map';
-export declare function useAttribution(map: Map, attribution: string | null | undefined): void;
--- @react-leaflet/core/types/grid-layer.d.ts
+++
@@ -1,2 +0,0 @@
-import { GridLayer, GridLayerOptions } from 'leaflet-rotate-map';
-export declare function updateGridLayer<E extends GridLayer, P extends GridLayerOptions>(layer: E, props: P, prevProps: P): void;
